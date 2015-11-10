7247f17179672cbea07072464840849b4852f813
owenhero <zehuihero@gmail.com>
2015-11-10 14:57:14 +0800

    add 拍照与录像
    增加了拍照与录像。使用了bmob后端云作为后端架构。使用了xUtils.

--- app/src/main/java/com/ak47/checkin_app/MainActivity.java
+++ app/src/main/java/com/ak47/checkin_app/MainActivity.java
@@ -1,22 +1,22 @@
 package com.ak47.checkin_app;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.ak47.checkin_app.R;
 
@@ -31,6 +31,9 @@ public class MainActivity extends AppCompatActivity  {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
+    private Button bnTakePhoto;
+//    private CoordinatorLayout coordinatorLayout;
+//    private RelativeLayout fgMain;
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -51,6 +54,11 @@ public class MainActivity extends AppCompatActivity  {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+//        fgMain = (RelativeLayout) mViewPager.findViewById(R.id.fgMain);
+//        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_content);
+
+//        bnTakePhoto = (Button) fgMain.findViewById(R.id.bnTakePhoto);
+//        Log.i(MainActivity.class.getSimpleName(), "我的button是" + bnTakePhoto.toString());
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
@@ -63,6 +71,7 @@ public class MainActivity extends AppCompatActivity  {
                         .setAction("Action", null).show();
             }
         });
+
 
     }
 
@@ -104,7 +113,8 @@ public class MainActivity extends AppCompatActivity  {
         public Fragment getItem(int position) {
             switch (position){
                 case 0:
-                    return new PlaceholderFragment();
+//                    return new PlaceholderFragment();
+                    return new MainFragment();
                 case 1:
                     return new ExploreFragment();
                 case 2:
